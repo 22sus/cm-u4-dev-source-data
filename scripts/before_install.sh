5f75221ded7e00889f66526c35b233016a1413a1
minu <78898539+22sus@users.noreply.github.com>
2025-12-05 15:16:47 +0900

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "[INFO] Cleaning up previous files..."
-# 기존 소스 코드만 삭제
 rm -f /home/ubuntu/api_server.py
 rm -f /home/ubuntu/requirements.txt
-# [삭제 금지] rm -rf /home/ubuntu/scripts  <-- 이 줄을 지우세요!
 exit 0
