36cf2f5d34d8fac911c3334b2b2ae3c7f6a7b8a4
minu <78898539+22sus@users.noreply.github.com>
2025-12-05 14:19:50 +0900

    Delete my-project directory

--- my-project/scripts/start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/flask-app
-# 백그라운드 실행, 로그는 app.log에 저장
-nohup python3 api_server.py > app.log 2>&1 &
--- my-project/scripts/stop_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# 기존에 실행 중인 api_server.py 찾아서 종료. 없어도 에러 안 나게 처리.
-pkill -f "api_server.py" || true
